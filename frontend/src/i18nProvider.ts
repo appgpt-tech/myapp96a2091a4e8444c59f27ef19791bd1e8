3ebc563454908a2b55b4cdc8dc16feeee94c733f
11ai <124818466+11ai@users.noreply.github.com>
2024-01-13 13:03:39 +0200

    created on Sat Jan 13 2024 13:03:21 GMT+0200 (Eastern European Standard Time)

---
+++ frontend/src/i18nProvider.ts
@@ -0,0 +1,17 @@
+// in i18nProvider.js
+    import { mergeTranslations } from "ra-core";
+    import polyglotI18nProvider from "ra-i18n-polyglot";
+    import enOriginal from 'ra-language-english';
+
+    const enResources = { resources: {"Albums":{"name":"albums","fields":{"Albumname":"Albumname","Numberoftracks":"Numberoftracks","Totalduration":"Totalduration","Id":"Id"}},"Artists":{"name":"artists","fields":{"Artistname":"Artistname","Dateofbirth":"Dateofbirth","Gender":"Gender","Band":"Band","Id":"Id"}},"Genres":{"name":"genres","fields":{"Genrename":"Genrename","Id":"Id"}},"Tracks":{"name":"tracks","fields":{"Name":"Name","Duration":"Duration","Album":"Album","Id":"Id"}}}};
+
+
+    const en = mergeTranslations(enOriginal,enResources);
+
+    const translations = { en};
+    export const i18nProvider = polyglotI18nProvider(
+      (locale) => translations[locale],
+      "en", //default locale
+      [{"locale":"en","name":"English"}]
+    );
+    
