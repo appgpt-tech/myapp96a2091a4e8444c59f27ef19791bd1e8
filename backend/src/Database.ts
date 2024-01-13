de7365d6867a939d7cef1e9ef07beb706a5f6a62
11ai <124818466+11ai@users.noreply.github.com>
2024-01-13 13:03:44 +0200

    created on Sat Jan 13 2024 13:03:21 GMT+0200 (Eastern European Standard Time)

---
+++ backend/src/Database.ts
@@ -0,0 +1,70 @@
+//Class to create tables and seed new database
+import { DataSource } from "typeorm";
+import { DBConfiguration } from "./Configuration";
+import { SettingsEntity } from "./db/Settings.entity";
+//autogenerate imports based on resources
+import { AlbumsEntity } from "./db/Albums.entity";
+import { ArtistsEntity } from "./db/Artists.entity";
+import { GenresEntity } from "./db/Genres.entity";
+import { TracksEntity } from "./db/Tracks.entity";
+
+export class Database {
+  static dbConfiguration: DBConfiguration;
+  public static ds: DataSource;
+
+  static async Initialize(dbConfiguration: DBConfiguration) {
+    Database.dbConfiguration = dbConfiguration;
+    let dbConfig: any = dbConfiguration as any;
+    //Autogenerate entities array from resource names
+
+    dbConfig.entities = [SettingsEntity, AlbumsEntity, ArtistsEntity, GenresEntity, TracksEntity];
+    Database.ds = new DataSource(dbConfig);
+    await Database.ds.initialize();
+
+    //TODO: Drop all tables
+
+
+    await Database.Seed();
+  }
+  static async Seed() {
+    let data: any = {"Albums":[],"Artists":[],"Genres":[],"Tracks":[]};
+    //Autogenerate multiple such calls ie for each resource and its data object
+    let isSeeded = await this.IsSeeded();
+    //if (!isSeeded) {
+    //forcing app recreation
+    if (true){
+      console.log('   Seeding database...');
+      await this.SeedResource("AlbumsEntity", data.Albums);
+await this.SeedResource("ArtistsEntity", data.Artists);
+await this.SeedResource("GenresEntity", data.Genres);
+await this.SeedResource("TracksEntity", data.Tracks); 
+      await this.SeedResource("SettingsEntity", {
+        settingname: "isSeeded",
+        settingvalue: "true",
+      });
+    }else{
+      console.log('   Database seeded already!');
+    }
+  }
+  static async IsSeeded() {
+    const repo = Database.ds.getRepository("SettingsEntity");
+    let rec: any = await repo.findOne({
+      select: {
+        settingname: true,
+        settingvalue: true,
+      },
+      where: {
+        settingname: "isSeeded",
+      },
+    });
+    if (rec && rec.settingvalue) return true;
+    return false;
+  }
+  static async SeedResource(resourceName: any, resourceData: any) {
+    const repo = Database.ds.getRepository(resourceName);
+    //await repo.clear();
+    console.log('   Seeding table '+resourceName);
+    await repo.upsert(resourceData, ["id"]);
+  }
+}
+
