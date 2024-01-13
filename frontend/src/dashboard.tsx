579ade58c7f1ce4e56aae3e9daf9fa8da631afa1
11ai <124818466+11ai@users.noreply.github.com>
2024-01-13 13:03:33 +0200

    created on Sat Jan 13 2024 13:03:21 GMT+0200 (Eastern European Standard Time)

---
+++ frontend/src/dashboard.tsx
@@ -0,0 +1,7 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+    export const Dashboard = () => (
+      <Card>
+      <CardHeader title= "myapp" />
+      <CardContent> music store < /CardContent>
+      </Card>
+    );
