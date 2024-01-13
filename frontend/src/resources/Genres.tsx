635c5b0b0a3aae2e5398eb5304b56c8bc7a100b1
11ai <124818466+11ai@users.noreply.github.com>
2024-01-13 13:03:36 +0200

    created on Sat Jan 13 2024 13:03:21 GMT+0200 (Eastern European Standard Time)

---
+++ frontend/src/resources/Genres.tsx
@@ -0,0 +1,81 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  //EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  //UrlInput,
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const GenresTitle = () => {
+  const record = useRecordContext();
+  return <span>Genres {record ? `"${ record.Genrename }"` : ""}</span>;
+};
+
+export const GenresList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="Genrename" />
+<NumberField source="Id" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const GenresEdit = () => (
+                    <Edit title={<GenresTitle />}>
+                      <SimpleForm>
+                          <TextInput source="Genrename"   />
+<NumberInput source="Id"   disabled/>
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const GenresCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="Genrename"   />
+<NumberInput source="Id"   disabled/>
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+
+    ];
+
+
