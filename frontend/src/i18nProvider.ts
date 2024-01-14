992a681b726a5adf4fe65bcee9826390a352dc08
11ai <124818466+11ai@users.noreply.github.com>
2024-01-15 01:18:12 +0200

    edited on Mon Jan 15 2024 01:17:59 GMT+0200 (Eastern European Standard Time)

--- frontend/src/i18nProvider.ts
+++ frontend/src/i18nProvider.ts
@@ -3,7 +3,7 @@
     import polyglotI18nProvider from "ra-i18n-polyglot";
     import enOriginal from 'ra-language-english';
 
-    const enResources = { resources: {"Albums":{"name":"albums","fields":{"Albumname":"Albumname","Numberoftracks":"Numberoftracks","Totalduration":"Totalduration","Id":"Id"}},"Artists":{"name":"artists","fields":{"Artistname":"Artistname","Dateofbirth":"Dateofbirth","Gender":"Gender","Band":"Band","Id":"Id"}},"Genres":{"name":"genres","fields":{"Genrename":"Genrename","Id":"Id"}},"Tracks":{"name":"tracks","fields":{"Name":"Name","Duration":"Duration","Album":"Album","Genre":"Genre","Id":"Id"}}}};
+    const enResources = { resources: {"Albums":{"name":"albums","fields":{"Albumname":"Albumname","Numberoftracks":"Numberoftracks","Totalduration":"Totalduration","Id":"Id"}},"Artists":{"name":"artists","fields":{"Artistname":"Artistname","Dateofbirth":"Dateofbirth","Gender":"Gender","Band":"Band","Id":"Id"}},"Genres":{"name":"genres","fields":{"Genrename":"Genrename","Id":"Id"}},"Tracks":{"name":"tracks","fields":{"Name":"Name","Duration":"Duration","Album":"Album","Id":"Id"}}}};
 
 
     const en = mergeTranslations(enOriginal,enResources);
